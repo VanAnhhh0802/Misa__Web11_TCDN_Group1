9cfad38bc265b79a36c7afb77295d38c9248240b
VanAnhhh0802 <hovananh2312@gmail.com>
2022-12-05 00:58:55 +0700

    feat: code  4 loai thong bao

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
-//  Hiển thị  active trên sidebar
-const itemActive = document.querySelectorAll(".menu-item__link");
+function initEvent() {
+  //Gán sự kiện hover hiện ra border
+  const inputElement = document.querySelector("#input-show");
+  const showBorders = document.querySelectorAll(".input__wrapper");
+  inputElement.addEventListener("focus", function () {
+    console.log("show");
+    //Bắt sự kiện khi focus vào ô input thì hiển thị border
+    for (const item of showBorders) {
+      item.target.classList.add("border-focus");
+    }
+  });
+
+  //   inputElement.addEventListener("blur", function () {
+  //     //Bắt sự kiện khi blur ra ngoài ô input thì ẩn border
+  //     for (const item of showBorders) {
+  //       item.classList.remove("border-hover");
+  //     }
+  //   });
+}
+initEvent();
